1da57b61fead4537b7c43cfd1e878f3f18081cb9
Mert Dümenci <mert@dumenci.me>
2012-08-03 13:32:58 +0300

    Initial Commit

---
+++ QuickReminders/MDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MDAppDelegate.h
+//  QuickReminders
+//
+//  Created by Mert on 8/3/12.
+//  Copyright (c) 2012 Mert Dumenci. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MDAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ QuickReminders/MDAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  MDAppDelegate.m
+//  QuickReminders
+//
+//  Created by Mert on 8/3/12.
+//  Copyright (c) 2012 Mert Dumenci. All rights reserved.
+//
+
+#import "MDAppDelegate.h"
+
+@implementation MDAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
